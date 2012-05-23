3c7fe32f30f871956af51922749025633a0d045b
friedger <friedger@gmail.com>
2012-05-23 13:24:10 +0200

    Fix for NPE on Samsung Tab 3.2

--- Safe/src/org/openintents/safe/wrappers/honeycomb/WrapActionBar.java
+++ Safe/src/org/openintents/safe/wrappers/honeycomb/WrapActionBar.java
@@ -6,35 +6,40 @@ import android.view.MenuItem;
 
 public class WrapActionBar {
 	private ActionBar mInstance;
-	
+
 	static {
 		try {
 			Class.forName("android.app.ActionBar");
-		} catch (Exception ex){
+		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
 	}
-	
+
 	/* calling here forces class initialization */
-	public static void checkAvailable() {}
-	
-	public WrapActionBar(Activity a){
+	public static void checkAvailable() {
+	}
+
+	public WrapActionBar(Activity a) {
 		mInstance = a.getActionBar();
 	}
-	
-	public void setDisplayHomeAsUpEnabled(boolean b){
-		mInstance.setDisplayHomeAsUpEnabled(b);
+
+	public void setDisplayHomeAsUpEnabled(boolean b) {
+		if (mInstance != null) {
+			mInstance.setDisplayHomeAsUpEnabled(b);
+		}
 	}
-	
-	public void setHomeButtonEnabled(boolean b){
-		mInstance.setHomeButtonEnabled(b);
+
+	public void setHomeButtonEnabled(boolean b) {
+		if (mInstance != null) {
+			mInstance.setHomeButtonEnabled(b);
+		}
 	}
-	
+
 	// show an icon in the actionbar if there is room for it.
-	public static void showIfRoom(MenuItem item){
+	public static void showIfRoom(MenuItem item) {
 		item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
 	}
-	
+
 	public static void invalidateOptionsMenu(Activity a) {
 		a.invalidateOptionsMenu();
 	}
